96428254d9ccf78d10ffd7eeb20a9eda483f828b
mariusbike1983 <pepumariusdoru@gmail.com>
2023-06-24 12:53:15 +0300

    improve the promises example

--- 3_promises/promises.js
+++ 3_promises/promises.js
@@ -15,19 +15,39 @@ function fetchTodos(url) {
     return promise;
 }
 
-function test() {    
-    fetch("https://dummyjson.com/todos?limit=5")
+function doFetchData(skip) {
+    return new Promise((resolve, reject) => {
+        fetch("https://dummyjson.com/todos?limit=5&skip="+skip)
+        .then(data => data.json())
+        .then(json => resolve(json.todos))
+        .catch(err => reject(err))
+    });
+}
+
+function test2() {
+    doFetchData(0)
+    .then(todos => console.log(0, "todos", todos))
+    .then(() => doFetchData(5))
+    .then(todos => console.log(5, "todos", todos))
+    .then(() => doFetchData(10))
+    .then(todos => console.log(10, "todos", todos))
+    .catch(err => console.log("err", err));
+}
+
+function test() {
+    fetch("https://dummyjson.com/todos?limit=5&skip="+0)
     .then(data => data.json()) // data.json() returns a new promise, thus we chain it to the stack
-    .then(json => console.log("todos", json.todos))
+    .then(json => console.log(skip, "todos", json.todos))
+
+    .then(() => fetch("https://dummyjson.com/todos?limit=5&skip="+5))
+    .then(data => data.json()) // data.json() returns a new promise, thus we chain it to the stack
+    .then(json => console.log(skip, "todos", json.todos))
+
+    .then(() => fetch("https://dummyjson.com/todos?limit=5&skip="+10))
+    .then(data => data.json()) // data.json() returns a new promise, thus we chain it to the stack
+    .then(json => console.log(skip, "todos", json.todos))
+
     .catch(err => console.log("err", err));
 }
 
-function testErr() {
-    fetchTodos("https://dummyjson.com/todoss?limit=5") // invalid URL, will throw an error
-    .then(data => data.json())
-    .then(todos => console.log("todos", todos))
-    .catch(err => console.log("err", err));
-}
-
-test();
-testErr();
+test2();
